0465e3f03c0df58b2c3d31439454a9177ee76d6a
Anton <32438821+AntonNordgren@users.noreply.github.com>
2025-04-24 11:22:24 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="app">
+
+      <div className="container">
+        <h1 className="title">This website sucks</h1>
+        <br />
+        <img className="picture" src="./stupid-cat.jpg" />
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
